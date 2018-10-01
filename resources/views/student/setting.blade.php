cab4c586854477ae14ac8c507f2643873800c4c5
internpoon <scpoon123@gmail.com>
2018-10-01 13:37:22 +0800

    Done setting.

--- resources/views/student/setting.blade.php
+++ resources/views/student/setting.blade.php
@@ -10,7 +10,7 @@
         <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
             <input class="form-control" type="text" name="email" placeholder="{{$user->email}}" readonly>
-            <small id="emailHelp" class="form-text text-muted">This field is cannot be modified.</small>
+            <small id="emailHelp" class="form-text text-muted">This field cannot be modified.</small>
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Current Password</label>
@@ -18,11 +18,11 @@
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">New Password</label>
-            <input type="password" class="form-control" name="newPassword" id="newPassword" placeholder="New Password">
+            <input type="password" class="form-control" name="password" id="password" placeholder="New Password">
         </div>
         <div class="form-group" id="pw">
             <label for="confirmPassword">Confirm Password</label>
-            <input type="password" class="form-control has-error" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password">
+            <input type="password" class="form-control has-error" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password">
             <small id="pwHelp" class="form-text text-muted" hidden style="color: #a94442;">Password not match !</small>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
@@ -32,8 +32,8 @@
 @section('custom_js')
 <script>
     $(document).ready(function () {
-        $('#newPassword, #confirmPassword').on('keyup', function () {
-            if ($('#newPassword').val() == $('#confirmPassword').val() || $('#confirmPassword').val() == '' ) {
+        $('#password, #password_confirmation').on('keyup', function () {
+            if ($('#password').val() == $('#password_confirmation').val() || $('#password_confirmation').val() == '' ) {
                 $('#pw').removeClass('has-error');
                 $('#pwHelp').hide()
             } else {
@@ -47,7 +47,14 @@
     <script>
         $(function() {
             toastr.error('{{ Session::get('error') }}');
-        })
+        });
+    </script>
+@endif
+@if(Session::has('success'))
+    <script>
+        $(function() {
+            toastr.success('{{ Session::get('success') }}');
+        });
     </script>
 @endif
 @endsection
